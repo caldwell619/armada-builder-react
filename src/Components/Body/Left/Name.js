3f68f450d8b1510445890d4abeaf9feda6828cb4
caldwell619 <caldwell8814@gmail.com>
2018-11-30 11:10:31 -0600

    pathing fix

--- src/Components/Body/Left/Name.js
+++ src/Components/Body/Left/Name.js
@@ -3,11 +3,7 @@ import React from 'react';
 const name = () => {
     return (
         <div className="row logo-container">
-<<<<<<< HEAD:src/Components/Body/Left/FleetDisplay/Name/Name.js
-            <img src={require("/Users/chriscaldwell/IdeaProjects/armada-builder-react/src/images/home-page/empire-logo.png")} alt="logo">
-=======
             <img src={"./images/home-page/empire-logo.png"} alt="logo">
->>>>>>> class-trial:src/Components/Body/Left/Name.js
             </img>
             <input type="text" name="fleet-name" placeholder="Name your Fleet" id="fleet-name"></input>
         </div>
