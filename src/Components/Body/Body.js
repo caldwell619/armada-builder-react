06c06fcec9b908be9e00e87dc0cf5604cc2ca6b4
caldwell619 <caldwell8814@gmail.com>
2018-11-29 18:43:33 -0600

    added comments before making a new change

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -3,20 +3,24 @@ import Left from './Left/Left'
 import Right from './Right/Right'
 
 class body extends React.Component {
+    //determining if the div is supposed to be shown
     state = {
         isShowing: false
     };
+    //function for changing the isShowing boolean on click
     changeShip =() => {
         const doesShow = this.state.isShowing;
         this.setState({isShowing: !doesShow});
     };
     render() {
+        //deciding whether or not to display the left side
         let left = null;
         if (!this.state.isShowing){
             left = (
                 <Left/>
             )
         }
+        //rendering the left, if the condition is met
         return (
             <main>
                 {left}
