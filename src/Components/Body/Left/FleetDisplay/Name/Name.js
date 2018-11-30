07dde4b8afb03e44d59d4aa96bbf6ae76594e634
caldwell619 <caldwell8814@gmail.com>
2018-11-29 19:18:33 -0600

    Fixed pathing issue to img

--- src/Components/Body/Left/FleetDisplay/Name/Name.js
+++ src/Components/Body/Left/FleetDisplay/Name/Name.js
@@ -3,7 +3,7 @@ import React from 'react';
 const name = () => {
     return (
         <div className="row logo-container">
-            <img src={require("/Users/chriscaldwell/IdeaProjects/armada-using-react/src/images/home-page/empire-logo.png")} alt="logo">
+            <img src={require("/Users/chriscaldwell/IdeaProjects/armada-builder-react/src/images/home-page/empire-logo.png")} alt="logo">
             </img>
             <input type="text" name="fleet-name" placeholder="Name your Fleet" id="fleet-name"></input>
         </div>
