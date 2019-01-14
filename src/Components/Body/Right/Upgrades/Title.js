bc34f1ebcc4c2bce3305fe95c6507739e3140b7f
caldwell619 <caldwell8814@gmail.com>
2019-01-14 15:18:48 -0600

    Significant refactoring. Built array to mover instead of using nested if's in return. Normalized data, utilized Switch in React Router to prevent dual rendering of pages that matched the same argument

--- src/Components/Body/Right/Upgrades/Title.js
+++ src/Components/Body/Right/Upgrades/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import {cards} from '../../../../data/cards.js'
+
+// import {cards.js} from '../../../../data/cards.js.js'
 
 class Upgrade extends React.Component {
     constructor(props) {
@@ -18,7 +19,7 @@ class Upgrade extends React.Component {
             upgrades.title = card;
             card.equipped = true;
             cards.forEach(titleCard => {
-                if (titleCard.title !== card.title){
+                if (titleCard.title !== card.title) {
                     titleCard.equipped = false;
                 }
 
@@ -44,57 +45,34 @@ class Upgrade extends React.Component {
             return index.id === this.props.match.params.id
         });
         const newShips = [...this.props.shipInfo];
-        const cardsData = [...this.props.cards.title];
-        return (
-            <div>
-                {cardsData.map((card) => {
-                        if (card.points < 400 - this.props.points) {
-                            //returning upgrade cards for every instance in the array
-                            //comparing against allowable ships
-                            card.ship.forEach(upgradeAvailability => {
-                                //if it exists, and the type === available
-                                if (newShips[shipIndex] && newShips[shipIndex].type === upgradeAvailability) {
-                                    if (card.equipped === false){
-                                        return (
-                                            <div className="ship-card span-1-of-3" key={card.id}>
-                                                <img src={`/images/cards/upgrades/title/${card.image}`}
-                                                     alt="img"
-                                                     onClick={this.addUpgradeHandler.bind(this, card)}/>
-                                            </div>
-                                        )
-                                    }
-                                    //getting here and not getting the right outcome
-                                }
-                            })
+        const targetShip = newShips[shipIndex];
+
+        const titleCards = [];
+        //building array to map over
+        this.props.upgradeCards.forEach(card => {
+            if (card.set === "title") {
+                if (card.points < 400 - this.props.points) {
+                    card.ship.forEach(typeShipAccepted => {
+                        if (targetShip && targetShip.type === typeShipAccepted) {
+                            titleCards.push(card);
                         }
+                    })
+                }
+            }
+        });
 
-                })})})})}
-
-                {/*{cards.title.map((card) => {*/}
-                {/*if (card.faction === "imperial" || !card.faction) {*/}
-                {/*if (card.points < 400 - this.props.points) {*/}
-                {/*const shipIndex = this.props.shipInfo.findIndex(index => {*/}
-                {/*return index.id === this.props.match.params.id*/}
-                {/*});*/}
-                {/*const newShips = [...this.props.shipInfo];*/}
-                {/*if (card.ship) {*/}
-                {/*return card.ship.map(upgradeAvailability => {*/}
-                {/*if (newShips[shipIndex] && newShips[shipIndex].type === upgradeAvailability) {*/}
-                {/*// does not render anything*/}
-                {/*return (*/}
-                {/*<div className="ship-card span-1-of-3" key={card.id}>*/}
-                {/*<img src={`/images/cards/upgrades/title/${card.image}`}*/}
-                {/*alt="img"*/}
-                {/*onClick={this.addUpgradeHandler.bind(this, card)}/>*/}
-                {/*</div>*/}
-                {/*)*/}
-                {/*}*/}
-                {/*})*/}
-                {/*}*/}
-                {/*}*/}
-                {/*}*/}
-                {/*})}*/}
-            </div>
+        return (
+         <div>
+             {titleCards.map(card => {
+                 return (
+                     <div className="ship-card span-1-of-3" key={card.id}>
+                         <img src={`/images/cards/upgrades/title/${card.image}`}
+                              alt={card.title}
+                              onClick={this.addUpgradeHandler.bind(this, card)}/>
+                     </div>
+                 )
+             })}
+         </div>
         )
     }
 }
