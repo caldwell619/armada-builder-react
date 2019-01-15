aa454a2f0a193726e03a0af681cc89bc78ee2b7e
caldwell619 <caldwell8814@gmail.com>
2019-01-14 22:45:54 -0600

    Stil can't figure out swapping upgrades across multiple ships. Intended functionality on the same ship, but issues persist on multiple instances

--- src/Components/Body/Right/Upgrades/Title.js
+++ src/Components/Body/Right/Upgrades/Title.js
@@ -7,36 +7,62 @@ class Upgrade extends React.Component {
         super();
     }
 
-    addUpgradeHandler = (card) => {
+    addUpgradeHandler = (givenCard) => {
+
+        //defining which ship to grab
         const shipIndex = this.props.shipInfo.findIndex(index => {
             return (index.id === this.props.match.params.id);
         });
         const newShips = [...this.props.shipInfo];
         const upgrades = newShips[shipIndex].upgrades;
         const currentPoints = this.props.points;
-        const cards = [...this.props.cards.title];
-        if (currentPoints + card.points < 400) {
-            upgrades.title = card;
-            card.equipped = true;
-            cards.forEach(titleCard => {
-                if (titleCard.title !== card.title) {
-                    titleCard.equipped = false;
-                }
+        const stateGivenCards = [...this.props.upgradeCards];
+        //equipping card
+        upgrades.title = givenCard;
 
-            });
+        //building array to map over and change equipped status
 
-        }
-        let counter = 0;
+        //creates array of upgrade cards
+        let equippedUpgrades = [];
         newShips.forEach(ship => {
-            counter += ship.points;
             Object.values(ship.upgrades).forEach(upgrade => {
-                if (upgrade != null) {
-                    counter += upgrade.points;
+                if (upgrade) {
+                    equippedUpgrades.push(upgrade);
                 }
             });
         });
 
-        this.props.upgrade(newShips, counter, cards);
+        let titleUpgrades = [];
+        equippedUpgrades.forEach(upgrade => {
+            if (upgrade.set === "title" && upgrade.unique === true) {
+                    titleUpgrades.push(upgrade);
+            }
+        });
+
+        //not working
+        stateGivenCards.forEach(card => {
+                titleUpgrades.forEach(equippedCard => {
+                    if (equippedCard.title === card.title) {
+                        card.equipped = true;
+                    }
+                    //sets false on cards that are being console logged in the above true condition
+                    else {
+                        card.equipped = false;
+                    }
+                })
+        });
+
+//adding up the points every time a ship is added
+        let counter = 0;
+        newShips.forEach(ship => {
+                counter += ship.points;
+                Object.values(ship.upgrades).forEach(upgrade => {
+                    if (upgrade != null) {
+                        counter+= upgrade.points;
+                    }
+                });
+            });
+        this.props.upgrade(newShips, counter, stateGivenCards);
     };
 
     render() {
@@ -50,29 +76,27 @@ class Upgrade extends React.Component {
         const titleCards = [];
         //building array to map over
         this.props.upgradeCards.forEach(card => {
-            if (card.set === "title") {
-                if (card.points < 400 - this.props.points) {
-                    card.ship.forEach(typeShipAccepted => {
-                        if (targetShip && targetShip.type === typeShipAccepted) {
-                            titleCards.push(card);
-                        }
-                    })
-                }
+            if (card.set === "title" && card.points < 400 - this.props.points) {
+                card.ship.forEach(typeShipAccepted => {
+                    if (targetShip && targetShip.type === typeShipAccepted && card.equipped === false) {
+                        titleCards.push(card);
+                    }
+                })
             }
         });
 
         return (
-         <div>
-             {titleCards.map(card => {
-                 return (
-                     <div className="ship-card span-1-of-3" key={card.id}>
-                         <img src={`/images/cards/upgrades/title/${card.image}`}
-                              alt={card.title}
-                              onClick={this.addUpgradeHandler.bind(this, card)}/>
-                     </div>
-                 )
-             })}
-         </div>
+            <div>
+                {titleCards.map(card => {
+                    return (
+                        <div className="ship-card span-1-of-3" key={card.id}>
+                            <img src={`/images/cards/upgrades/title/${card.image}`}
+                                 alt={card.title}
+                                 onClick={this.addUpgradeHandler.bind(this, card)}/>
+                        </div>
+                    )
+                })}
+            </div>
         )
     }
 }
