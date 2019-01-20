c457ad820f2eac2c0866f3a3857f7fbfeb376217
caldwell619 <caldwell8814@gmail.com>
2019-01-19 21:10:41 -0600

    created adding card method to allow for card equipping

--- src/Components/Body/Right/Upgrades/GenericUpgrade.js
+++ src/Components/Body/Right/Upgrades/GenericUpgrade.js
@@ -2,57 +2,55 @@ import React from 'react';
 import {upgradeCards} from '../../../../data/cards.js'
 
 
-
 class GenericUpgrade extends React.Component {
     constructor(props) {
         super();
     }
 
     addUpgradeHandler = (card) => {
-        //finding the index of targeted ship
+        //defining necessary variables
         const shipIndex = this.props.shipInfo.findIndex(index => {
             return (index.id === this.props.match.params.id);
         });
         const newShips = [...this.props.shipInfo];
+        const upgradeCategory = this.props.match.params.upgradeType;
+        const targetedShip = newShips[shipIndex];
 
-        const currentPoints = this.props.points;
+        //equipping card
+        targetedShip.upgrades[upgradeCategory] = card;
 
-        let upgradeCards = [...this.props.upgradeCards];
-
-
-        if (currentPoints + card.points < 400) {
-            // newShips[shipIndex].upgrades[""]
-            let counter = 0;
-            newShips.forEach(ship => {
-                counter += ship.points;
-                Object.values(ship.upgrades).forEach(upgrade => {
-                    if (upgrade != null) {
-                        counter += upgrade.points;
-                    }
-                });
+        //counting up points
+        let counter = 0;
+        newShips.forEach(ship => {
+            counter += ship.points;
+            Object.values(ship.upgrades).forEach(upgrade => {
+                if (upgrade != null) {
+                    counter += upgrade.points;
+                }
             });
-            //sending back the new values
-            this.props.upgrade(newShips, counter, upgradeCards);
-        }
+        });
+
+        //sending back the new values
+        this.props.upgrade(newShips, counter, upgradeCards, card);
     };
 
-    render(){
+    render() {
         const upgradeType = this.props.match.params.upgradeType;
         const selectedUpgradeCards = [];
         upgradeCards.forEach(card => {
-            if (card.set === upgradeType){
+            if (card.set === upgradeType) {
                 selectedUpgradeCards.push(card);
             }
         });
         return (
             <div>
                 {selectedUpgradeCards.map(card => {
-                        return (
-                            <div className="ship-card span-1-of-3" key={card.id}>
-                                <img src={`/images/cards/upgrades/${upgradeType}/${card.image}`} alt={card.title}
-                                     onClick={this.addUpgradeHandler.bind(this, card)}/>
-                            </div>
-                        )
+                    return (
+                        <div className="ship-card span-1-of-3" key={card.id}>
+                            <img src={`/images/cards/upgrades/${upgradeType}/${card.image}`} alt={card.title}
+                                 onClick={this.addUpgradeHandler.bind(this, card)}/>
+                        </div>
+                    )
                 })
                 }
             </div>
