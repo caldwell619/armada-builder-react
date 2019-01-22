d272b6100cfc8ca43e03d72d85c6e31b92483417
caldwell619 <caldwell8814@gmail.com>
2019-01-22 08:02:52 -0600

    Normalizedish card display data, working on a better solution

--- src/Components/Body/Right/Upgrades/Commanders.js
+++ src/Components/Body/Right/Upgrades/Commanders.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {cards} from "../../../../data/cards";
 
 
 class Commander extends React.Component {
@@ -14,54 +13,34 @@ class Commander extends React.Component {
         const newShips = [...this.props.shipInfo];
         const upgradeCards = [...this.props.upgradeCards];
 
-        let upgradeType = card.set;
+        let clickedCard = card;
         let upgrades = newShips[shipIndex].upgrades;
-        let currentPoints = this.props.points;
-        if (currentPoints + card.points < 400) {
-            upgrades.commander = card;
-            if (card.title === "Darth Vader"){
-                upgradeCards.forEach(upgrade => {
-                    if (card.title === upgrade.title){
-                        console.log("meeting criteria:");
-                        console.log(upgrade);
-                        upgrade.equipped = true;
-                        upgrade.available = false;
-                    }
-                })
-            }
-        }
 
 
-        let counter = 0;
-        newShips.forEach(ship => {
-            counter += ship.points;
-            Object.values(ship.upgrades).forEach(upgrade => {
-                if (upgrade != null) {
-                    counter += upgrade.points;
-                }
-            });
-        });
+        upgrades.commander = card;
 
-        this.props.upgrade(newShips, counter, upgradeCards, upgradeType);
+
+        this.props.upgrade(newShips, upgradeCards, clickedCard);
     };
 
     render() {
+        let commanderCards = [];
+        this.props.upgradeCards.forEach(card => {
+            if (card.set === "commander" && card.faction === "imperial") {
+                commanderCards.push(card);
+            }
+        });
         return (
             <div>
-                {this.props.upgradeCards.map(commander => {
-                    if (commander.set === "commander") {
-                        if (commander.faction === "imperial") {
-                            if (commander.points < 400 - this.props.points) {
-                                if (this.props.commanderChosen === false && commander.available === true){
-                                    console.log(commander);
-                                    return (
-                                            <div className="ship-card span-1-of-3" key={commander.id}>
-                                                <img src={`/images/cards/upgrades/commander/${commander.image}`} alt="img"
-                                                     onClick={this.addUpgradeHandler.bind(this, commander)}/>
-                                            </div>
-                                        )
-                                }
-                            }
+                {commanderCards.map(commander => {
+                    if (commander.points < 400 - this.props.points) {
+                        if (this.props.commanderChosen === false && commander.available === true) {
+                            return (
+                                <div className="ship-card span-1-of-3" key={commander.id}>
+                                    <img src={`/images/cards/upgrades/commander/${commander.image}`} alt={commander.title}
+                                         onClick={this.addUpgradeHandler.bind(this, commander)}/>
+                                </div>
+                            )
                         }
                     }
                 })}
