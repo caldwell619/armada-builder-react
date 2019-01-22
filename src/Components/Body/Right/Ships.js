76cfbf91128555754c1cbe28aed74a5207b1ca37
caldwell619 <caldwell8814@gmail.com>
2019-01-22 08:06:09 -0600

    Removed redundant points counting

--- src/Components/Body/Right/Ships.js
+++ src/Components/Body/Right/Ships.js
@@ -1,53 +1,51 @@
 import React from 'react';
-import {cards} from '../../../data/cards.js'
+import {shipCards} from '../../../data/cards.js'
+
 let uniqid = require('uniqid');
 
 
-class ship extends React.Component {
+class Ships extends React.Component {
     constructor(props) {
         super(props);
     }
 
     addShip = (ship) => {
+
         //define new state
-        //pass down props
         let updatedShips = [...this.props.shipInfo];
-        let currentPoints = this.props.points;
+        // let currentPoints = this.props.points;
         let counter = 0;
-        //stopping the addition of new ships if points exceed max
-        if (currentPoints + ship.points < 400){
-            //refactor this later
+        let chosenShip = {
+            name: ship.title,
+            points: ship.points,
+            id: uniqid(),
+            imagePath: `/images/cards/ship/imperial/${ship.image}`,
+            upgrades: {...ship.upgrades},
+            type: ship.type,
+            dual: ship.dual,
+            upgradesShown: false,
+            size: ship.size
+        };
+        updatedShips.push(chosenShip);
+        updatedShips.forEach(ship => {
+            counter += ship.points;
+        });
 
-                let chosenShip = {
-                name: ship.title,
-                points: ship.points,
-                id: uniqid(),
-                imagePath: `/images/cards/ship/imperial/${ship.image}`,
-                upgrades: {...ship.upgrades},
-                type: ship.type,
-                dual: ship.dual,
-                upgradesShown: false
-            };
-                updatedShips.push(chosenShip);
-                updatedShips.forEach(ship => {
-                    counter += ship.points;
-                });
 
-        }
         //using function to change the state before setting it
         //asynchronous behavior requires the setState to  be a function
         this.setState((state, props) => {
             props.click(updatedShips, counter);
-         return ({
-                 selectedShips: updatedShips,
+            return ({
+                selectedShips: updatedShips,
                 totalPoints: counter
-                })
+            })
         });
     };
 
-    render (){
+    render() {
         let noMoreShipsDiv = null;
-        if (400 - this.props.points < 22){
+        if (400 - this.props.points < 22) {
             noMoreShipsDiv = (
                 <div className="crawl-container">
                     <div className="fade"/>
@@ -61,25 +59,29 @@ class ship extends React.Component {
                 </div>
             )
         }
-
+        let empireShips = [];
+        shipCards.forEach(ship => {
+            if (ship.faction === "imperial") {
+                if (ship.points < 400 - this.props.points) {
+                    empireShips.push(ship);
+                }
+            }
+        });
         return (
             <div className="cards-container">
-                {cards.ship.map(ship => {
-                    if (ship.faction === "imperial") {
-                        if (ship.points < 400 - this.props.points){
-                            return (
-                                <div className="ship-card span-1-of-3" key={ship.id}>
-                                    <img src={"/images/cards/ship/imperial/" + ship.image} alt="card" onClick={this.addShip.bind(this, ship)}/>
-                                </div>
-                            )
-                        }
-                    }
+                {empireShips.map(ship => {
+                    return (
+                        <div className="ship-card span-1-of-3" key={ship.id}>
+                            <img src={`/images/cards/ship/imperial/${ship.image}`} alt={ship.title}
+                                 onClick={this.addShip.bind(this, ship)}/>
+                        </div>
+                    )
                 })}
                 {noMoreShipsDiv}
             </div>
         )
     }
 
-};
+}
 
-export default ship;
+export default Ships;
