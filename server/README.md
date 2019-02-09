bc8f25a818f9e3cfdcce5585a2798a33c3346c86
caldwell619 <caldwell8814@gmail.com>
2019-02-09 17:16:30 -0600

    changed structure to allow deployment

--- server/README.md
+++
@@ -1,4 +0,0 @@
-# Muster the Fleet 
-A fullstack, MongoDB, Express, React, and NodeJS stack application for building a fleet to play Star Wars: Armada
-
-## Express Server
--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: "563548367394-3gbb2rlael2dhert2rf9crj5jrsrfdid.apps.googleusercontent.com",
-    googleClientSecret: "gqoymvixKQpPVz_YlDlpZRg4",
-    mongoURI: "mongodb://chris:chris1@ds221155.mlab.com:21155/armada-builder-dev",
-    cookieKey: ",fkjbEBFg23r9dSASFFF2sdvsvsFCASasac1p3bfbBEfpgwevnxdmo1hr29h"
-};
