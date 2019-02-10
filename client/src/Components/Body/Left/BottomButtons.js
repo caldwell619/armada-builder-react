9b01c2b2999a50626fe57edcab0e846cf5260935
caldwell619 <caldwell8814@gmail.com>
2019-02-09 19:02:48 -0600

    changed save button to reflect log in status

--- client/src/Components/Body/Left/BottomButtons.js
+++ client/src/Components/Body/Left/BottomButtons.js
@@ -22,27 +22,41 @@ class BottomButtons extends Component{
             })
         };
 
-    render(){
-        let button = null;
-        if (this.props.fleetName !== ""){
-            button = (
-                <Link to={"/profile"} onClick={this.postShips}><button id="save-button" className="save-button">Save</button></Link>
-            )
-        } else {
-            button = (
-                <div className="save-button-global-cont">
-                    <div className="save-text">
-                        <p>Please name your fleet before saving</p>
-                    </div>
-                    <div className="save-button-cont">
-                        <button id="disabled-save-button" className="save-button" disabled={true}>Save</button>
-                    </div>
-                </div>
-            )
+    textRender = () => {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <p>Login or sign up to save fleets</p>
+                );
+            default:
+                let button = null;
+                if (this.props.fleetName !== ""){
+                    button = (
+                        <Link to={"/profile"} onClick={this.postShips}><button id="save-button" className="save-button">Save</button></Link>
+                    )
+                } else {
+                    button = (
+                        <div className="save-button-global-cont">
+                            <div className="save-text">
+                                <p>Please name your fleet before saving</p>
+                            </div>
+                            <div className="save-button-cont">
+                                <button id="disabled-save-button" className="save-button" disabled={true}>Save</button>
+                            </div>
+                        </div>
+                    )
+                }
+                return button;
         }
+    };
+
+    render(){
+
         return (
             <div className="button-action-bar">
-                {button}
+                {this.textRender()}
             </div>
         )
     }
