8d1cd6b070bccbcdabcf722966ff8d2487364702
caldwell619 <caldwell8814@gmail.com>
2019-02-25 21:39:08 -0600

    starting faceoff

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from './Components/Header/Header'
 import Body from './Components/Body/Body'
 import Landing from './Components/Body/Landing'
 import Login from './Components/Body/Login'
 import {Link, Route} from 'react-router-dom';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import * as actions from './store/actions';
-import { BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Profile from "./Components/Body/Profile";
 import Contact from "./Components/Body/Contact";
 import ShowFleet from "./Components/Body/ShowFleet";
+import FaceOffLanding from "./Components/Body/FaceOffLanding";
 
 class App extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchUser();
         fetch("/api/user").then(result => console.log(result))
     }
 
-  render() {
+    render() {
 
-    return (
-      <div className="App">
-          <BrowserRouter>
-              <div>
-                  <Header/>
-
-                      <Route path='/builder/:faction' render={(routeProps) => <Body {...routeProps}/>}/>
-                      <Route path='/login' render={() => <Login/>}/>
-                      <Route path='/profile' exact render={() => <Profile/>}/>
-                      <Route path='/profile/edit/:fleetId' render={(routeProps) => <ShowFleet {...routeProps}/>}/>
-                      <Route path='/contact' render={() => <Contact/>}/>
-                      <Route path='/' exact render={(routeProps) => <Landing {...routeProps}/>}/>
-              </div>
-          </BrowserRouter>
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <React.Fragment>
+                        <Header/>
+                        <Route path='/builder/:faction' render={(routeProps) => <Body {...routeProps}/>}/>
+                        <Route path='/login' render={() => <Login/>}/>
+                        <Route path='/profile' exact render={() => <Profile/>}/>
+                        <Route path='/profile/edit/:fleetId' render={(routeProps) => <ShowFleet {...routeProps}/>}/>
+                        <Route path='/contact' render={() => <Contact/>}/>
+                        <Route path='/face-off' exact render={() => <FaceOffLanding/>}/>
+                        <Route path='/' exact render={(routeProps) => <Landing {...routeProps}/>}/>
+                    </React.Fragment>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
